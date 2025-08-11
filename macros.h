f039cd7ae152775792354007abf25883e4755b01
RedPaladin7 <ishans7325@gmail.com>
2025-08-11 19:38:45 +0530

    first commit

---
+++ macros.h
@@ -0,0 +1,21 @@
+#pragma once 
+
+#include <cstring>
+#include <iostream>
+
+using namespace std;
+
+#define LIKELY(x) __builtin_expect(!!(x), 1)
+#define UNLIKELY(x) __builtin_expect(!!(x), 0)
+
+inline auto ASSERT(bool cond, const string& msg) noexcept {
+    if(UNLIKELY(!cond)){
+        cerr<<"ASSERT : "<< msg <<endl;
+    }
+    exit(EXIT_FAILURE);
+}
+
+inline auto FAILURE(const string& msg) noexcept{
+    cerr<<"FATAL : "<<msg<<endl;
+    exit(EXIT_FAILURE);
+}
---
+++ thread_utils.h
@@ -0,0 +1,44 @@
+#pragma once 
+
+#include <iostream>
+#include <atomic>
+#include <thread>
+#include <unistd.h>
+#include <sys/syscall.h>
+
+using namespace std;
+
+namespace Common {
+    inline auto setThreadCore(int core_id) noexcept {
+        cpu_set_t cpuset;
+        CPU_ZERO(&cpuset);
+        CPU_SET(core_id, &cpuset);
+        
+        return (pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t), &cpuset)==0);
+    }
+    template<typename T, typename... A>
+    inline auto createAndStartThread(int code_id, const string& name, T && func, A &&... args) noexcept {
+        atomic<bool> running(false), failed(false);
+        auto thread_body = [&] {
+            if(core_id >= 0 && !setThreadCore(core_id)){
+                cerr<<"Failed to set core affinity for "<<name<<" "<<pthread_self()<<" to "<<core_id<<endl;
+                failed = true;
+                return;
+            }
+            cout<<"Set core affinity for "<<name<<" "<<pthread_self()<<" to "<<core_id<<end;
+            running = true;
+            forward<T>(func)((forward<A>(args))...);
+        };
+        auto t = new thread(thread_body);
+        while(!running && !failed){
+            using namespace literals::chrono_literals;
+            this_thread::sleep_for(1s);
+        }
+        if(failed){
+            t->join();
+            delete t;
+            t = nullptr;
+        }
+        return t;
+    }
+}
